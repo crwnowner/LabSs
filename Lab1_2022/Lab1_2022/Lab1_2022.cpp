27c456761927ffc229f13f63e7b58c184ff884a4
crwnowner <109227529+crwnowner@users.noreply.github.com>
2022-10-05 18:23:24 +0300

    Add files via upload

---
+++ Lab1_2022/Lab1_2022/Lab1_2022.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{   //Заполняю массив числами от 1 до n, n - количество элементов в нем
+    const int n = 50;
+    int list_of_numbers[n+1];
+    for (int i = 0; i <= n; i++) {
+        list_of_numbers[i] = i;
+    }
+
+    //Первое простое число: simple_number = 2
+    int simple_number = 2;
+    //Зануляю составные числа в массиве, сначала квадраты
+    for (int i = simple_number; i * i <= n; i++) {
+        if (list_of_numbers[i] != 0) {
+            //Потом прочесываю прибавляя несколько раз простые числа
+            for (int j = i * i; j <= n; j += i) {
+                list_of_numbers[j] = 0;
+            }
+        }
+    }
+
+    //вывожу не зануленные числа
+    cout << "\n";
+    for (int i = 0; i < n; i++) {
+        if (list_of_numbers[i] != 0) {
+            cout << list_of_numbers[i] << "\n";
+        }
+    }
+}
+
